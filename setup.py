0f89666659160e9f831ece612be716a6cf9fa087
Tres Bailey <tres.bailey@gmail.com>
2013-03-09 00:27:24 -0500

    Setup reads from requirements.txt

--- setup.py
+++ setup.py
@@ -1,5 +1,12 @@
 from setuptools import setup
 
+fn = os.path.join(os.path.dirname(__file__), 'requirements.txt')
+reqs_list = list()
+with open(fn, 'r') as reqs:
+    for line in reqs.readlines():
+        reqs_list.append(line)
+
+
 setup(name='YourAppName', version='1.0',
       description='OpenShift Python-2.7 Community Cartridge based application',
       author='Your Name', author_email='ramr@example.org',
@@ -7,9 +14,5 @@ setup(name='YourAppName', version='1.0',
 
       #  Uncomment one or more lines below in the install_requires section
       #  for the specific client drivers/modules your application needs.
-      install_requires=['greenlet', 'gevent',
-                        #  'MySQL-python',
-                        #  'pymongo',
-                        #  'psycopg2',
-      ],
+      install_requires=reqs_list,
      )
