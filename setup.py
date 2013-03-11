49a576178420b5297005c3ff56199119c9abf7dd
Tres Bailey <tres.bailey@gmail.com>
2013-03-10 22:36:49 -0400

    Requirements back into set up.py

--- setup.py
+++ setup.py
@@ -1,12 +1,6 @@
 import os
 from setuptools import setup
 
-fn = os.path.join(os.path.dirname(__file__), 'wsgi/requirements.txt')
-reqs_list = list()
-with open(fn, 'r') as reqs:
-    for line in reqs.readlines():
-        reqs_list.append(line)
-
 
 setup(name='YourAppName', version='1.0',
       description='OpenShift Python-2.7 Community Cartridge based application',
@@ -15,5 +9,27 @@ setup(name='YourAppName', version='1.0',
 
       #  Uncomment one or more lines below in the install_requires section
       #  for the specific client drivers/modules your application needs.
-      install_requires=reqs_list,
+      install_requires=[
+            Babel,
+            Flask,
+            Flask-MongoAlchemy,
+            Flask-Uploads,
+            Genshi,
+            Jinja2,
+            MongoAlchemy,
+            Werkzeug,
+            decorator,
+            distribute,
+            ethtool,
+            firstboot,
+            gps,
+            iniparse,
+            nose,
+            pymongo,
+            python-dateutil,
+            redis,
+            simplejson,
+            virtualenv,
+            wsgiref
+          ]
      )
