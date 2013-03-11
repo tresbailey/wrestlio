96b82e18298521c14c3377fdced6f43f89bc86c0
Tres Bailey <tres.bailey@gmail.com>
2013-03-10 22:55:45 -0400

    Adding flaskext

--- setup.py
+++ setup.py
@@ -10,8 +10,9 @@ setup(name='YourAppName', version='1.0',
       #  Uncomment one or more lines below in the install_requires section
       #  for the specific client drivers/modules your application needs.
       install_requires=[ 'Flask==0.8', 'MongoAlchemy==0.11',
-        'Werkzeug==0.8.3', 'nose==1.0.0',
+        'Werkzeug==0.8.3',
         'pymongo==2.1.1', 'python-dateutil==1.5',
         'redis==2.4.11', 'simplejson==2.1.6', 
-        'virtualenv==1.6.4', 'wsgiref==0.1.2']
+        'virtualenv==1.6.4', 'wsgiref==0.1.2',
+        'Flask-MongoAlchemy==0.5.3']
      )
