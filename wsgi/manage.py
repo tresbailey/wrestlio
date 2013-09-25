f4056a8b7db516937b410e09d97840f001652126
tresbailey <tres.bailey@gmail.com>
2013-09-24 23:06:02 -0400

    manage change to build

--- wsgi/manage.py
+++ wsgi/manage.py
@@ -1,8 +1,6 @@
 import os
 from wrestling import app
-from wrestling.middlewares.auth import OAuthMiddleware
 
 
 if __name__ == '__main__':
-    app.wsgi_app = OAuthMiddleware(app.wsgi_app)
     app.run(host="localhost", port=5001)
