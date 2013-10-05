5dffe60b7a6174229160f959f674c11cc209c08b
tresbailey <tres.bailey@gmail.com>
2013-10-04 23:36:28 -0400

    New dir

--- wsgi/wrestling/logs.py
+++ wsgi/wrestling/logs.py
@@ -2,7 +2,7 @@ import logging
 from logging import handlers
 import os
 
-file_loc = os.path.join(os.environ.get('OPENSHIFT_HOME_DIR', '.'), 'wrestling.log')
+file_loc = os.path.join(os.environ.get('OPENSHIFT_HOME_DIR', '.'), 'python', 'logs',  'wrestling.log')
 handler = logging.handlers.RotatingFileHandler(file_loc)
 log = logging.getLogger('wrestling')
 log.setLevel("DEBUG")
