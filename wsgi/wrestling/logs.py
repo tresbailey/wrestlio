4d6e2749d39c4f2ec36b0bdffa42510beb600591
tresbailey <tres.bailey@gmail.com>
2013-10-04 23:28:07 -0400

    Added diff log dir

--- wsgi/wrestling/logs.py
+++ wsgi/wrestling/logs.py
@@ -1,7 +1,9 @@
 import logging
 from logging import handlers
+import os
 
-handler = logging.handlers.RotatingFileHandler("wrestling.log")
+file_loc = os.path.poin(os.environ.get('OPENSHIFT_HOME_DIR', '.'), 'wrestling.log')
+handler = logging.handlers.RotatingFileHandler(file_loc)
 log = logging.getLogger('wrestling')
 log.setLevel("DEBUG")
 log.addHandler( handler)
