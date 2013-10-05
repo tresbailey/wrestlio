599caae27b3850ee7b33f11bed15080511f23fec
tresbailey <tres.bailey@gmail.com>
2013-10-04 23:40:33 -0400

    Adding requirements to setup

--- setup.py
+++ setup.py
@@ -10,9 +10,10 @@ setup(name='YourAppName', version='1.0',
       #  Uncomment one or more lines below in the install_requires section
       #  for the specific client drivers/modules your application needs.
       install_requires=[ 'Flask==0.8', 'MongoAlchemy==0.11',
-        'Werkzeug==0.8.3',
+        'Werkzeug==0.8.3', 'Flask-Login==0.1.3',
+        'Flask-OpenID==1.1.1', 'Flask-Principal==0.4.0', 'Flask-WTF==0.8.3',
         'pymongo==2.1.1', 'python-dateutil==1.5',
         'redis==2.4.11', 'simplejson==2.1.6', 
-        'wsgiref==0.1.2',
+        'wsgiref==0.1.2', 'python-openid==2.2.5', 'blinker==1.3',
         'Flask-MongoAlchemy==0.5.3', 'flask_oauth==0.12']
      )
