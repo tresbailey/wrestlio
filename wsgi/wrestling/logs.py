7dcf935138277810441c3dbd2db7fea62e5a1780
tresbailey <tres.bailey@gmail.com>
2013-10-04 23:29:56 -0400

    Mistyped letter

--- wsgi/wrestling/logs.py
+++ wsgi/wrestling/logs.py
@@ -2,7 +2,7 @@ import logging
 from logging import handlers
 import os
 
-file_loc = os.path.poin(os.environ.get('OPENSHIFT_HOME_DIR', '.'), 'wrestling.log')
+file_loc = os.path.join(os.environ.get('OPENSHIFT_HOME_DIR', '.'), 'wrestling.log')
 handler = logging.handlers.RotatingFileHandler(file_loc)
 log = logging.getLogger('wrestling')
 log.setLevel("DEBUG")
